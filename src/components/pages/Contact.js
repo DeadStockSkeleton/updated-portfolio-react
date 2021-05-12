5bce44af98e3b7097a63f9bc5c641421130eda2b
Iyana Medlock <iyanamedlock@gmail.com>
2021-05-11 23:08:43 -0500

    finsihed

--- src/components/pages/Contact.js
+++ src/components/pages/Contact.js
@@ -3,16 +3,13 @@ function Contact() {
         <div id="contact" className="container section p-4">
         <div class="container-fluid d-flex">
                     <a class="m-2 text-muted" href="https://www.linkedin.com/in/i-yana-medlock-93a0831b7">
-                        <i class="fab fa-linkedin-in"></i>
+                        <i class="fab contact-icon fa-linkedin-in"></i>
                     </a>
                     <a class="m-2 text-muted" href="https://github.com/DeadStockSkeleton">
-                        <i class="fab fa-github"></i>
+                        <i class="fab contact-icon fa-github"></i>
                     </a>
                     <a class="m-2 text-muted" href="tel:832-513-9561">
-                        <i class="fas fa-envelope"></i>
-                    </a>
-                    <a class="m-2 text-muted" href="tel:832-513-9561">
-                        <i class="fas fa-phone-square"></i>
+                        <i class="fas contact-icon fa-phone-square"></i>
                     </a>
                 </div>
     </div>
